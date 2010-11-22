a6bff9365c22d0b651218c14a30acd92b1ab275a
Your Name <getitnowmarketing@gmail.com>
2010-11-22 13:24:09 -0500

    first commit

---
+++ extracommands.h
@@ -0,0 +1,18 @@
+extern int do_verify;
+
+void
+do_verify_switch();
+
+/*int
+__system(const char *command);
+
+int
+format_non_mtd_device(const char* root);
+
+void usb_toggle_emmc();
+*/
+void usb_toggle_sdcard();
+
+void key_logger_test();
+
+
