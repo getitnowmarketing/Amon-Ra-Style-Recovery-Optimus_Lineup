9a87c0a75883cb96ce44797c7af5a748317869b1
Your Name <getitnowmarketing@gmail.com>
2011-11-16 00:02:47 -0500

    add support for touch scrolling thanks rbox1 and change key mapping

---
+++ default_recovery_ui.c
@@ -0,0 +1,137 @@
+
+
+#include <linux/input.h>
+#include "recovery_ui_keys.h"
+
+#ifdef USE_TOUCH_SCROLLING
+#include "minui/minui.h"
+#endif
+
+
+#ifdef DEFAULT_RECOVERY_UI_KEYS
+
+int device_handle_key(int key_code, int visable) {
+	if (visable) {
+		switch (key_code) {
+			case KEY_VOLUMEDOWN:
+			
+#ifdef USE_TOUCH_SCROLLING
+			case MT_FAKE_DN:
+#endif
+				return HIGHLIGHT_DOWN;
+		
+			case KEY_VOLUMEUP:
+			
+#ifdef USE_TOUCH_SCROLLING
+			case MT_FAKE_UP:
+#endif
+				return HIGHLIGHT_UP;
+
+			case KEY_POWER:
+			case KEY_MENU:
+				return SELECT_ITEM;
+
+			case KEY_BACK:
+				return GO_BACK;
+		}
+	}
+	return NO_ACTION;
+}
+
+#endif
+
+#ifdef ALOHA_RECOVERY_UI_KEYS
+
+int device_handle_key(int key_code, int visable) {
+	if (visable) {
+		switch (key_code) {
+			case KEY_VOLUMEDOWN:
+			
+#ifdef USE_TOUCH_SCROLLING
+			case MT_FAKE_DN:
+#endif
+				return HIGHLIGHT_DOWN;
+		
+			case KEY_VOLUMEUP:
+			
+#ifdef USE_TOUCH_SCROLLING
+			case MT_FAKE_UP:
+#endif
+				return HIGHLIGHT_UP;
+
+			case KEY_POWER:
+			case KEY_END:
+				return SELECT_ITEM;
+
+			case KEY_BACK:
+				return GO_BACK;
+		}
+	}
+	return NO_ACTION;
+}
+
+#endif
+
+
+#ifdef HTC_NO_TRACKBALL_RECOVERY_UI_KEYS
+
+int device_handle_key(int key_code, int visable) {
+	if (visable) {
+		switch (key_code) {
+			case KEY_VOLUMEDOWN:
+			case KEY_MENU:
+#ifdef USE_TOUCH_SCROLLING
+			case MT_FAKE_DN:
+#endif
+				return HIGHLIGHT_DOWN;
+		
+			case KEY_VOLUMEUP:
+			case KEY_HOME:
+#ifdef USE_TOUCH_SCROLLING
+			case MT_FAKE_UP:
+#endif
+				return HIGHLIGHT_UP;
+
+			case KEY_POWER:
+			case KEY_SEARCH:
+				return SELECT_ITEM;
+
+			case KEY_BACK:
+				return GO_BACK;
+		}
+	}
+	return NO_ACTION;
+}
+
+#endif
+
+#ifdef HTC_TRACKBALL_RECOVERY_UI_KEYS
+
+int device_handle_key(int key_code, int visable) {
+	if (visable) {
+		switch (key_code) {
+			case KEY_DOWN:
+#ifdef USE_TOUCH_SCROLLING
+			case MT_FAKE_DN:
+#endif
+				return HIGHLIGHT_DOWN;
+		
+			case KEY_UP:
+#ifdef USE_TOUCH_SCROLLING
+			case MT_FAKE_UP:
+#endif
+				return HIGHLIGHT_UP;
+
+			case KEY_POWER:
+			case BTN_MOUSE:
+				return SELECT_ITEM;
+
+			case KEY_BACK:
+			case VOLUME_DOWN:
+				return GO_BACK;
+		}
+	}
+	return NO_ACTION;
+}
+
+#endif
